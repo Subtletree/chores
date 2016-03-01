679d725a3aba4e3df61004318bf86362d1f0a20c
Ryan Scott <ryanscott161@gmail.com>
2016-03-02 12:12:56 +1300

    add api namespace

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -2,7 +2,7 @@ import DS from 'ember-data';
 import ENV from "../config/environment";
 
 export default  DS.JSONAPIAdapter.extend({
-  //namespace: 'api',
+  namespace: 'api',
   // if your rails app is on a different port from your ember app
   // this can be helpful for development.
   // in production, the host for both rails and ember should be the same.
