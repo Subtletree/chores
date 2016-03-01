864cc1360e84615f9cdc11d51ccd7229fa79442a
Ryan Scott <ryanscott161@gmail.com>
2016-03-02 00:15:35 +1300

    set host

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,7 +1,9 @@
 import Ember from 'ember';
+import config from '../config/environment';
 
 export default Ember.Route.extend({
   model() {
+    var host = config.host || '';
     return this.store.findAll('job');
   }
 });
--- config/environment.js
+++ config/environment.js
@@ -2,6 +2,7 @@
 
 module.exports = function(environment) {
   var ENV = {
+    host: 'http://api.scottbase.ryanscott.co.nz'
     modulePrefix: 'chores',
     environment: environment,
     baseURL: '/',
